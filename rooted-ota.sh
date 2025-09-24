e083ab07c0637dec4e7acaab568570ac7ac5efe6
Sifl46 <77085292+Sifl46@users.noreply.github.com>
2025-09-24 14:34:32 -0700

    Update rooted-ota.sh

--- rooted-ota.sh
+++ rooted-ota.sh
@@ -283,9 +283,9 @@ function downloadAndVerifyFromChenxiaolong() {
     curl --fail -sL "${url}" > "${downloadedZipFile}"
     curl --fail -sL "${url}.sig" > "${downloadedZipFile}.sig"
     
-    # Validate against author's public key
-    #ssh-keygen -Y verify -I chenxiaolong -f <(echo "chenxiaolong $CHENXIAOLONG_PK") -n file \
-      #-s "${downloadedZipFile}.sig" < "${downloadedZipFile}"
+     Validate against author's public key
+    ssh-keygen -Y verify -I chenxiaolong -f <(echo "chenxiaolong $CHENXIAOLONG_PK") -n file \
+      -s "${downloadedZipFile}.sig" < "${downloadedZipFile}"
     
     echo N | unzip "${downloadedZipFile}" -d .tmp
     rm "${downloadedZipFile}"*
